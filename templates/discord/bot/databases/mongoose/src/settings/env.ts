95bc0902ab6f4c0b610f9f3d1b0497848a87ede4
Rincko <guilhermericardopro71@gmail.com>
2024-04-17 17:36:50 -0400

    database presets env fix

--- templates/discord/bot/databases/mongoose/src/settings/env.ts
+++ templates/discord/bot/databases/mongoose/src/settings/env.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-    BOT_TOKEN: z.string({ description: "Discord Bot Token is required" }).readonly(),
+    BOT_TOKEN: z.string({ description: "Discord Bot Token is required" }).min(1),
     WEBHOOK_LOGS_URL: z.string().url().optional(),    
-    MONGO_URI: z.string({ description: "MongoDb URI is required" })
+    MONGO_URI: z.string({ description: "MongoDb URI is required" }).min(1)
 });
 
 type EnvSchema = z.infer<typeof envSchema>;
--- templates/discord/bot/databases/typesaurus/src/settings/env.ts
+++ templates/discord/bot/databases/typesaurus/src/settings/env.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-    BOT_TOKEN: z.string({ description: "Discord Bot Token is required" }).readonly(),
+    BOT_TOKEN: z.string({ description: "Discord Bot Token is required" }).min(1),
     WEBHOOK_LOGS_URL: z.string().url().optional(),
-    FIREBASE_PATH: z.string({ description: "Firebase account path is required" })
+    FIREBASE_PATH: z.string({ description: "Firebase account path is required" }).min(1)
 });
 
 type EnvSchema = z.infer<typeof envSchema>;
