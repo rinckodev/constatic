239bc4106b20c75ee66f4cf1e5abbfc007d0563c
rinckodev <guilhermericardopro71@gmail.com>
2025-11-13 02:05:48 -0400

    feat(base) add command structures

---
+++ packages/base/src/creators/commands/command.ts
@@ -0,0 +1,191 @@
+import { ApplicationCommandOptionType, ApplicationCommandType, AutocompleteInteraction, ChatInputCommandInteraction, MessageContextMenuCommandInteraction, UserContextMenuCommandInteraction, type ApplicationCommandOptionAllowedChannelTypes, type ApplicationCommandOptionChoiceData, type BaseApplicationCommandData, type CacheType, type LocalizationMap } from "discord.js";
+
+export type CommandType = Exclude<
+    ApplicationCommandType,
+    ApplicationCommandType.PrimaryEntryPoint
+>;
+
+type AutocompleData<Type> = Promise<
+    | readonly ApplicationCommandOptionChoiceData<
+        Type extends (number | string) ? Type : (number | string)
+    >[]
+    | undefined
+    | void
+>;
+
+export type AutocompleteRun<Type, Perm> = (
+    this: void,
+    interaction: AutocompleteInteraction<CacheMode<Perm>>
+) => AutocompleData<Type>;
+
+interface AutocompleteOptionData<Type, Perm> {
+    autocomplete?: true | AutocompleteRun<Type, Perm>;
+}
+
+interface BaseOptionData {
+    name: string;
+    nameLocalizations?: LocalizationMap;
+    description?: string;
+    descriptionLocalizations?: LocalizationMap;
+    required?: boolean;
+}
+
+interface StringOptionData<Perm> extends
+    BaseOptionData, AutocompleteOptionData<string, Perm> {
+    type: ApplicationCommandOptionType.String,
+    choices?: readonly ApplicationCommandOptionChoiceData<string>[];
+    minLength?: number;
+    maxLength?: number;
+}
+
+interface NumberOptionData<P> extends
+    BaseOptionData, AutocompleteOptionData<number, P> {
+    type:
+    | ApplicationCommandOptionType.Number
+    | ApplicationCommandOptionType.Integer;
+    choices?: readonly ApplicationCommandOptionChoiceData<number>[];
+    minValue?: number;
+    maxValue?: number;
+}
+
+interface ChannelOptionData extends BaseOptionData {
+    type: ApplicationCommandOptionType.Channel
+    channelTypes?: readonly ApplicationCommandOptionAllowedChannelTypes[]
+}
+interface CommonOptionData extends BaseOptionData {
+    type:
+    | ApplicationCommandOptionType.Attachment
+    | ApplicationCommandOptionType.Boolean
+    | ApplicationCommandOptionType.Mentionable
+    | ApplicationCommandOptionType.Role
+    | ApplicationCommandOptionType.User
+}
+
+export type SlashCommandPrimitiveOptionData<Perm> =
+    | StringOptionData<Perm>
+    | NumberOptionData<Perm>
+    | CommonOptionData
+    | ChannelOptionData;
+
+export interface SubCommandOptionData<Perm> extends Omit<BaseOptionData, "required"> {
+    type: ApplicationCommandOptionType.Subcommand,
+    options?: SlashCommandPrimitiveOptionData<Perm>[]
+}
+
+export interface GroupOptionData<Perm> extends Omit<BaseOptionData, "required"> {
+    type: ApplicationCommandOptionType.SubcommandGroup,
+    options: SubCommandOptionData<Perm>[]
+}
+
+type CacheMode<Perm> = Perm extends false ? "cached" : CacheType;
+
+interface RunThis {
+    /**
+     * Blocks the flow of executions
+     */
+    block(): never;
+}
+
+type ResolveCommandModuleData<Return> = Return extends void ? undefined : Return;
+
+export type SubCommandModuleData<Perm, Return> =
+    Omit<BaseOptionData, "required"> & {
+        group?: string;
+        run(
+            this: RunThis,
+            interaction: ChatInputCommandInteraction<CacheMode<Perm>>,
+            data: ResolveCommandModuleData<Return>
+        ): Promise<void>;
+        options?: SlashCommandPrimitiveOptionData<Perm>[]
+    };
+
+export type SubCommandGroupModuleData<Perm, Return, T> =
+    Omit<BaseOptionData, "required"> & {
+        options?: Omit<SubCommandOptionData<Perm>, "type">[]
+        run?(
+            this: RunThis,
+            interaction: ChatInputCommandInteraction<CacheMode<Perm>>,
+            data: ResolveCommandModuleData<Return>
+        ): Promise<T>;
+    };
+
+type RunInteraction<T, P> =
+    T extends ApplicationCommandType.Message
+    ? MessageContextMenuCommandInteraction<CacheMode<P>> :
+    T extends ApplicationCommandType.User
+    ? UserContextMenuCommandInteraction<CacheMode<P>> :
+    ChatInputCommandInteraction<CacheMode<P>>
+
+type BaseAppCommandData =
+    & Omit<BaseApplicationCommandData, "dmPermission">
+    & Pick<BaseOptionData, "descriptionLocalizations">
+
+export interface CommandData<T, P, R> extends BaseAppCommandData {
+    name: string;
+    description?: string;
+    dmPermission?: P;
+    type?: T;
+    global?: boolean;
+    run?(this: RunThis, interaction: RunInteraction<T, P>): Promise<R>;
+    autocomplete?: AutocompleteRun<string | number, P>;
+    options?:
+    | SlashCommandPrimitiveOptionData<P>[]
+    | (GroupOptionData<P> | SubCommandOptionData<P>)[];
+}
+
+export type SlashCommandOptionData<Perm> =
+    | SlashCommandPrimitiveOptionData<Perm>
+    | GroupOptionData<Perm>
+    | SubCommandOptionData<Perm>
+
+export type CommandModule =
+    | (SubCommandGroupModuleData<unknown, unknown, unknown> & {
+        type: ApplicationCommandOptionType.SubcommandGroup
+    })
+    | (SubCommandModuleData<unknown, unknown> & {
+        type: ApplicationCommandOptionType.Subcommand
+        group?: string
+    });
+
+class GroupCommandModule<Type, Perm, Return, ModuleReturn> {
+    constructor(
+        public readonly command: Command<Type, Perm, Return>,
+        public readonly data: SubCommandGroupModuleData<Perm, Return, ModuleReturn>
+    ) { }
+    public subcommand(data: SubCommandModuleData<Perm, ModuleReturn>) {
+        data.group ??= this.data.name;
+        this.command.subcommand(data);
+    }
+}
+
+export class Command<Type, Perm, Return> {
+    public readonly modules: CommandModule[] = []
+    constructor(
+        public readonly data: CommandData<Type, Perm, Return>
+    ) {
+        this.data.type ??= <Type>ApplicationCommandType.ChatInput
+        this.data.dmPermission ??= <Perm>false;
+        if (this.data.type === ApplicationCommandType.ChatInput){
+            this.data.description??=this.data.name;
+            this.data.name = this.data.name
+                .toLowerCase()
+                .replaceAll(" ", "");
+        }
+        if (this.data.name.length > 32){
+            this.data.name = this.data.name.slice(0, 32);
+        }
+    }
+    public group<ModuleReturn>(data: SubCommandGroupModuleData<Perm, Return, ModuleReturn>) {
+        this.modules.push({ ...data, 
+            type: ApplicationCommandOptionType.SubcommandGroup
+        });
+        return new GroupCommandModule<Type, Perm, Return, ModuleReturn>(
+            this, data
+        );
+    }
+    public subcommand<Return>(data: SubCommandModuleData<Perm, Return>) {
+        this.modules.push({ ...data, 
+            type: ApplicationCommandOptionType.Subcommand
+        });
+    }
+}
---
+++ packages/base/src/creators/commands/manager.ts
@@ -0,0 +1,158 @@
+import { ApplicationCommandOptionType, ApplicationCommandType, type ApplicationCommandOptionData, type ApplicationCommandSubCommandData, type ApplicationCommandSubGroupData, type ChatInputApplicationCommandData, type MessageApplicationCommandData, type UserApplicationCommandData } from "discord.js";
+import type { Command, CommandModule, SlashCommandOptionData } from "./command.js";
+
+type Runner = Function | null | undefined;
+
+type BuildedCommandData = (
+    | UserApplicationCommandData
+    | MessageApplicationCommandData
+    | ChatInputApplicationCommandData
+) & { global?: boolean };
+
+export class CommandManager {
+    private readonly collection = new Map<string, Command<unknown, unknown, unknown>>();
+    public readonly runners = new Map<string, Runner[]>();
+    public set<T, P, R>(name: string, command: Command<T, P, R>) {
+        this.collection.set(name, command);
+    }
+    public build() {
+        const commands = Array.from(this.collection.values());
+        return commands.map(command => {
+            const {
+                options: rawOptions, description,
+                descriptionLocalizations, ...data
+            } = command.data;
+
+            const path = `/${data.type}/${data.name}`;
+
+            const modulesOptions = this.buildModules(
+                command.modules, path, data.run
+            );
+
+            const options = this.buildOptions([
+                ...rawOptions ?? [], ...modulesOptions
+            ], path);
+
+            const slashData = data.type === ApplicationCommandType.ChatInput
+                ? {
+                    description, descriptionLocalizations,
+                    ...(options.length >= 1 ? { options } : {})
+                }
+                : {}
+
+            return { ...data, ...slashData };
+        }) as BuildedCommandData[];
+    }
+    private buildOptions(options: SlashCommandOptionData<boolean>[], path: string) {
+        const resolved: ApplicationCommandOptionData[] = [];
+        for (const option of options) {
+            const description = option.description ?? option.name;
+
+            if (
+                "autocomplete" in option &&
+                option.autocomplete &&
+                typeof option.autocomplete === "function"
+            ) {
+                this.runners.set(
+                    `${path}/${option.name}`,
+                    [option.autocomplete]
+                );
+            }
+
+            switch (option.type) {
+                case ApplicationCommandOptionType.SubcommandGroup: {
+                    const { options: subcommands, ...data } = option;
+
+                    resolved.push({
+                        ...data, description,
+                        options: this.buildOptions(
+                            subcommands, `${path}/${data.name}`
+                        ) as ApplicationCommandSubCommandData[]
+                    });
+                    continue;
+                }
+                case ApplicationCommandOptionType.Subcommand: {
+                    const { options, ...data } = option;
+                    resolved.push({
+                        ...data,
+                        description, ...(options?.length ? {
+                            options: this.buildOptions(
+                                options, `${path}/${data.name}`
+                            ) as Exclude<
+                                ApplicationCommandOptionData,
+                                | ApplicationCommandSubGroupData
+                                | ApplicationCommandSubCommandData
+                            >[]
+                        } : {})
+                    });
+                    continue;
+                }
+                case ApplicationCommandOptionType.String:
+                case ApplicationCommandOptionType.Integer:
+                case ApplicationCommandOptionType.Number: {
+                    const { choices, autocomplete, ...data } = option;
+
+                    const validation = data.type === ApplicationCommandOptionType.String
+                        ? { minLength: data.minLength, maxLength: data.maxLength }
+                        : { minValue: data.minValue, maxValue: data.maxValue }
+
+                    const extra = autocomplete
+                        ? { autocomplete: true, ...validation }
+                        : choices?.length
+                            ? { choices: choices.slice(0, 25) }
+                            : validation;
+
+                    resolved.push(Object.assign({
+                        ...data, description, ...extra
+                    }));
+                    continue;
+                }
+                default: {
+                    resolved.push({ ...option, description });
+                }
+            }
+        }
+        return resolved;
+    }
+    private buildModules(modules: CommandModule[], path: string, run?: Function): SlashCommandOptionData<boolean>[] {
+        const resolved: SlashCommandOptionData<boolean>[] = [];
+        if (!modules.length) return resolved;
+
+        const groups = modules.filter(module =>
+            module.type === ApplicationCommandOptionType.SubcommandGroup
+        );
+        const subcommands = modules.filter(module =>
+            module.type === ApplicationCommandOptionType.Subcommand
+        );
+
+        if (groups.length >= 1) {
+            for (const group of groups) {
+                const data = [
+                    ...(group.options ?? []).map(data => ({
+                        ...data,
+                        type: ApplicationCommandOptionType.Subcommand as const,
+                    })),
+                    ...subcommands.filter(
+                        sub => sub.group === group.name
+                    )
+                ]
+                resolved.push({ ...group, options: data });
+
+                for (const subcommand of data) {
+                    this.runners.set(
+                        `${path}/${group.name}/${subcommand.name}`,
+                        [run, group.run, (subcommand as CommandModule).run]
+                    );
+                }
+            };
+        }
+        for (const subcommand of subcommands.filter(sub => !sub.group)) {
+            this.runners.set(
+                `${path}/${subcommand.name}`,
+                [run, subcommand.run]
+            );
+            resolved.push(subcommand);
+        }
+        return resolved;
+    }
+}
