cdd580d3f350bbbe8c26014f6b5f4d42d2b90974
rinckodev <guilhermericardopro71@gmail.com>
2025-11-18 01:35:20 -0400

    chore(base): organize creators imports

--- packages/base/src/creators/index.ts
+++ packages/base/src/creators/index.ts
@@ -1,62 +1,19 @@
-import type { ApplicationCommandType, CacheType, ClientEvents, PermissionResolvable } from "discord.js";
-import { ConstaticApp, type BaseCommandsConfig, type BaseEventsConfig, type BaseRespondersConfig } from "../app.js";
-import { Command, type CommandData, type CommandType } from "./commands/command.js";
-import { Event, type EventData } from "./events/event.js";
-import { Responder, type ResponderData, type ResponderType } from "./responders/responder.js";
+export {
+    ResponderType,
+    type Responder,
+    type ResponderData,
+    type ResponderInteraction
+} from "./responders/responder.js";
 
-interface SetupCreatorsOptions {
-    commands?: BaseCommandsConfig & {
-        defaultMemberPermissions?: PermissionResolvable[];
-    };
-    responders?: Partial<BaseRespondersConfig>;
-    events?: Partial<BaseEventsConfig>;
-}
+export {
+    type Command,
+    type CommandData
+} from "./commands/command.js";
 
-export function setupCreators(options: SetupCreatorsOptions = {}) {
-    const app = ConstaticApp.getInstance();
-    app.config.commands = { ...options.commands ??= {} };
-    app.config.commands.guilds ??= [];
+export {
+    type Event,
+    type EventData,
+    type EventPropData
+} from "./events/event.js";
 
-    if (process.env.GUILD_ID?.length) {
-        app.config.commands.guilds.push(
-            process.env.GUILD_ID
-        );
-    }
-    return {
-        createCommand<
-            T extends CommandType = ApplicationCommandType.ChatInput,
-            P extends boolean = false,
-            R = void
-        >(data: CommandData<T, P, R>): Command<T, P, R> {
-            if (options.commands?.defaultMemberPermissions){
-                data.defaultMemberPermissions??=options.commands
-                    .defaultMemberPermissions;
-            }
-            const command = new Command(data);
-            app.commands.set(command);
-            return command;
-        },
-        createEvent: function <
-            EventName extends keyof ClientEvents
-        >(data: EventData<EventName>) {
-            if (data.event === "clientReady") {
-                data.once = true;
-            }
-            const event = new Event(data);
-            app.events.add(event);
-            return event;
-        },
-        createResponder<
-            Path extends string,
-            const Types extends readonly ResponderType[],
-            Parsed,
-            Cache extends CacheType = CacheType
-        >(data: ResponderData<Path, Types, Parsed, Cache>) {
-            const responder = new Responder(data);
-            app.responders.set(responder);
-            return responder;
-        },
-    }
-}
-
-export { ResponderType } from "./responders/responder.js";
+export * from "./setup.js";
---
+++ packages/base/src/creators/setup.ts
@@ -0,0 +1,61 @@
+import type { ApplicationCommandType, CacheType, ClientEvents, PermissionResolvable } from "discord.js";
+import { ConstaticApp, type BaseCommandsConfig, type BaseEventsConfig, type BaseRespondersConfig } from "../app.js";
+import { Command, type CommandData, type CommandType } from "./commands/command.js";
+import { Event, type EventData } from "./events/event.js";
+import { Responder, type ResponderData, type ResponderType } from "./responders/responder.js";
+
+interface SetupCreatorsOptions {
+    commands?: BaseCommandsConfig & {
+        defaultMemberPermissions?: PermissionResolvable[];
+    };
+    responders?: Partial<BaseRespondersConfig>;
+    events?: Partial<BaseEventsConfig>;
+}
+
+export function setupCreators(options: SetupCreatorsOptions = {}) {
+    const app = ConstaticApp.getInstance();
+    app.config.commands = { ...options.commands ??= {} };
+    app.config.commands.guilds ??= [];
+
+    if (process.env.GUILD_ID?.length) {
+        app.config.commands.guilds.push(
+            process.env.GUILD_ID
+        );
+    }
+    return {
+        createCommand<
+            T extends CommandType = ApplicationCommandType.ChatInput,
+            P extends boolean = false,
+            R = void
+        >(data: CommandData<T, P, R>): Command<T, P, R> {
+            if (options.commands?.defaultMemberPermissions){
+                data.defaultMemberPermissions??=options.commands
+                    .defaultMemberPermissions;
+            }
+            const command = new Command(data);
+            app.commands.set(command);
+            return command;
+        },
+        createEvent: function <
+            EventName extends keyof ClientEvents
+        >(data: EventData<EventName>) {
+            if (data.event === "clientReady") {
+                data.once = true;
+            }
+            const event = new Event(data);
+            app.events.add(event);
+            return event;
+        },
+        createResponder<
+            Path extends string,
+            const Types extends readonly ResponderType[],
+            Parsed,
+            Cache extends CacheType = CacheType
+        >(data: ResponderData<Path, Types, Parsed, Cache>) {
+            const responder = new Responder(data);
+            app.responders.set(responder);
+            return responder;
+        },
+    }
+}
+
