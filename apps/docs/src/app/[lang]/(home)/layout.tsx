b801cfde8a37f4f32c65b74654957574643bfca9
rinckodev <guilhermericardopro71@gmail.com>
2026-02-12 05:13:15 -0400

    chore(docs): remove blog link

--- apps/docs/src/app/[lang]/(home)/layout.tsx
+++ apps/docs/src/app/[lang]/(home)/layout.tsx
@@ -1,5 +1,5 @@
-import { HomeLayout } from "fumadocs-ui/layouts/home";
 import { baseOptions } from "@lib/layout.shared";
+import { HomeLayout } from "fumadocs-ui/layouts/home";
 
 export default function Layout({ children }: LayoutProps<"/[lang]">) {
   return <HomeLayout {...baseOptions()}>{children}</HomeLayout>;
--- apps/docs/src/lib/layout.shared.tsx
+++ apps/docs/src/lib/layout.shared.tsx
@@ -1,9 +1,8 @@
 import { ConstaticTitle } from "@/components/constatic/title";
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 import { FaBook } from "react-icons/fa";
-import { FaNewspaper } from "react-icons/fa6";
-import { i18n } from "./i18n";
 import { MdOutlineUpdate } from "react-icons/md";
+import { i18n } from "./i18n";
 
 export function baseOptions(): BaseLayoutProps {
   return {
@@ -17,18 +16,13 @@ export function baseOptions(): BaseLayoutProps {
     githubUrl: "https://github.com/rinckodev/constatic",
     links: [
       {
-        text: <span className="flex gap-2 items-center"><FaBook />Docs</span>,
+        text: <span className="flex gap-2 items-center"><FaBook/>Docs</span>,
         url: "/docs",
         active: "nested-url",
       },
       {
-        text: <span className="flex gap-2 items-center"><MdOutlineUpdate />Changelog</span>,
+        text: <span className="flex gap-2 items-center"><MdOutlineUpdate size={20} />Changelog</span>,
         url: "/changelog",
-        active: "nested-url",
-      },
-      {
-        text: <span className="flex gap-2 items-center"><FaNewspaper />Blog</span>,
-        url: "/blog",
         active: "nested-url",
       },
     ],
